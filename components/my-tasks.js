9942e1cb28ed682e47c8d64d9fad3dab22d592f2
Eugene Van Zyl <eugevanz@gmail.com>
2022-03-06 03:54:59 +0200

    import supabase from "../context/auth-context";

--- components/my-tasks.js
+++ components/my-tasks.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import moment from "moment";
 import { useEffect, useState } from "react";
-import supabase from "../../context/auth-context";
+import supabase from "../context/auth-context";
 
 function MyTasks() {
   const router = useRouter();
@@ -28,7 +28,8 @@ function MyTasks() {
                 className="uk-link-toggle"
                 href="#list-item"
                 onClick={() => router.push(`/tasks/${item.id}`)}
-                data-uk-scroll>
+                data-uk-scroll
+              >
                 <div className="uk-text-bold uk-link-text">
                   {item.name.toUpperCase()}
                 </div>
